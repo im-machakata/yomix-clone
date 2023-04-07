221f9effdc1f5685f2a6944a9ffb4f7bf89fcc73
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-04-07 09:16:59 +0200

    clean: rearrange files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// import css
+import './src/assets/style.css';
+
+// import views
+import App from './pages/demo/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

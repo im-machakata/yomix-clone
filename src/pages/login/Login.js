82ecc3258ec0088a39a6d4347cda4000b82a95db
Isaac Machakata <isaac@caasi.co.zw>
2023-04-27 08:51:53 +0200

    backup

---
+++ src/pages/login/Login.js
@@ -0,0 +1,12 @@
+// components import
+import Header from '../../components/Header.js'
+
+function App() {
+  return (
+    <div className='application'>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;

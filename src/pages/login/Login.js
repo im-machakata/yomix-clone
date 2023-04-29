1132149c9f14c86111dba3d1b04f3428e22a4bff
Isaac Machakata <isaac@caasi.co.zw>
2023-04-29 17:39:23 +0200

    style: login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,23 +1,38 @@
-// components import
+import { useState } from 'react';
+import Style from './Login.css'
 import Header from '../../components/LoginHeader.js'
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const isBtnEnabled = () => {
+    return username.length > 1 && password.length > 1;
+  }
+  const btnClasses = () => {
+    return isBtnEnabled() ? '' : 'disabled';
+  }
   return (
     <div className='application'>
       <Header></Header>
       <section className='login-container'>
         <div className='text-input'>
           <div className='input-image'></div>
-          <input type='text' placeholder='Username' />
+          <input type='text' placeholder='Username' onChange={value => { setUsername(value) }} />
         </div>
         <div className='text-input'>
           <div className='input-image'></div>
-          <input type='text' placeholder='Password' />
-          <a>Forgot?</a>
+          <input type='text' placeholder='Password' onChange={value => { setPassword(value) }} />
+          <a href='/' className='nav-item'>Forgot?</a>
         </div>
-        <button className='button'>Login</button>
+        <button className={btnClasses()}>Login</button>
+        <div className='additional-info'>
+          <div><a href='/'>Yomix Verification Code to Login</a></div>
+          <div>New User? <a href='/' className='color-primary'>Sign Up Now</a></div>
+        </div>
       </section>
-      <footer></footer>
+      <footer className='login-footer'>
+        Econet Wireless Zimbabwe
+      </footer>
     </div>
   );
 }
